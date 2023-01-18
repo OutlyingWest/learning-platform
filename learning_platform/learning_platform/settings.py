43acfe7a97b7c5e733d003c9f8fcfecf2c08e725
OutlyingWest <alexeybuv7@amail.com>
2023-01-18 19:08:02 +0300

    secret key put to env var, timezone and lang changed

--- learning_platform/learning_platform/settings.py
+++ learning_platform/learning_platform/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+7qv48!ht^6z=e3ngz*07-mw61ccm%c)e$1w^wz7a97t7vldk='
+SECRET_KEY = os.environ.get('SECRET_KEY_LP')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -110,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
