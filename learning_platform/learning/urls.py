c9e2e94195078ecec322caf25ec6b0b08ca0c016
OutlyingWest <alexeybuv7@amail.com>
2023-01-24 18:59:47 +0300

    regex added to learning/detail url

--- learning_platform/learning/urls.py
+++ learning_platform/learning/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index),
     path('create/', views.create, name='create'),
-    path('delete/<int:course_id>', views.delete, name='delete'),
-    path('detail/<int:course_id>', views.detail, name='detail'),
-    path('enroll/<int:course_id>', views.enroll, name='enroll'),
+    path('delete/<int:course_id>/', views.delete, name='delete'),
+    re_path('^detail/(?P<course_id>[1-9]+[0-9]*)/$', views.detail, name='detail'),
+    path('enroll/<int:course_id>/', views.enroll, name='enroll'),
 ]
