415b9d2bac95725c22608cac1bf07e9f7c6eab37
OutlyingWest <alexeybuv7@amail.com>
2023-01-28 13:53:14 +0300

    static paths added

--- learning_platform/learning_platform/settings.py
+++ learning_platform/learning_platform/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'learning_platform.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/img',
+    BASE_DIR / 'static/styles',
+    BASE_DIR / 'static/scripts',
+]
 
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
