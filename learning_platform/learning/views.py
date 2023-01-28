ee88be1866d8604001878c0760a8af01c43e20b8
OutlyingWest <alexeybuv7@amail.com>
2023-01-28 13:48:22 +0300

    index and detail views wrote

--- learning_platform/learning/views.py
+++ learning_platform/learning/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
+from .models import Course, Lesson
 
 
 def index(request):
-    return HttpResponse('Список всех доступных курсов')
+    courses = Course.objects.all()
+    current_year = datetime.now().year
+    return render(request,
+                  context={
+                      'courses': courses,
+                      'current_year': current_year,
+                  },
+                  template_name='index.html')
 
 
 def create(request):
@@ -15,10 +24,16 @@ def delete(request, course_id):
 
 
 def detail(request, course_id):
-    return HttpResponse(f'Детальная информация о курсе id: {course_id}')
+    course = Course.objects.get(id=course_id)
+    lessons = Lesson.objects.filter(course=course_id)
+    context = {
+        'course': course,
+        'lessons': lessons
+    }
+    return render(request,
+                  context=context,
+                  template_name='detail.html')
 
 
 def enroll(request, course_id):
     return HttpResponse(f'Здесь можно записаться на выбранный курс id: {course_id}')
-
-
