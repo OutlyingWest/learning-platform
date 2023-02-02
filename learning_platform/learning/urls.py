1ea6509a2377853fad4a6d6c255af773e84f2eeb
OutlyingWest <alexeybuv7@amail.com>
2023-02-02 13:56:27 +0300

    CreateView and Detail View work completely

--- learning_platform/learning/urls.py
+++ learning_platform/learning/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.MainView.as_view(), name='index'),
     path('create/', views.create, name='create'),
     path('delete/<int:course_id>/', views.delete, name='delete'),
-    re_path('^detail/(?P<course_id>[1-9]+[0-9]*)/$', views.detail, name='detail'),
+    re_path('^detail/(?P<course_id>[1-9]+[0-9]*)/$', views.CourseDetailView.as_view(), name='detail'),
     path('enroll/<int:course_id>/', views.enroll, name='enroll'),
 ]
--- learning_platform/learning/views.py
+++ learning_platform/learning/views.py
@@ -1,33 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from datetime import datetime
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Course, Lesson, Tracking
+from .forms import CourseForm
 
 
-def index(request):
-    courses = Course.objects.all()
-    current_year = datetime.now().year
-    return render(request,
-                  context={
-                      'courses': courses,
-                      'current_year': current_year,
-                  },
-                  template_name='index.html')
+class MainView(ListView):
+    template_name = 'index.html'
+    queryset = Course.objects.all()
+    context_object_name = 'courses'
+
+    paginate_by = 3
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(MainView, self).get_context_data(**kwargs)
+        context['current_year'] = datetime.now().year
+        return context
 
 
-def create(request):
-    if request.method == 'POST':
-        data = request.POST
-        Course.objects.create(title=data['title'],
-                              author=request.user,
-                              description=data['description'],
-                              start_date=data['start_date'],
-                              duration=data['duration'],
-                              price=data['price'],
-                              count_lessons=data['count_lessons'])
-        return redirect('index')
-    else:
-        return render(request, 'create.html')
+class CourseCreateView(CreateView):
+    template_name = 'create.html'
+    # Model of course to create
+    model = Course
+    form_class = CourseForm
+
+    def get_success_url(self):
+        return reverse()
 
 
 def delete(request, course_id):
@@ -35,16 +34,19 @@ def delete(request, course_id):
     return redirect('index')
 
 
-def detail(request, course_id):
-    course = Course.objects.get(id=course_id)
-    lessons = Lesson.objects.filter(course=course_id)
-    context = {
-        'course': course,
-        'lessons': lessons,
-    }
-    return render(request,
-                  context=context,
-                  template_name='detail.html')
+class CourseDetailView(DetailView):
+    template_name = 'detail.html'
+    context_object_name = 'course'
+    # Redefine name of default url parameter "pk" to "course_id"
+    pk_url_kwarg = 'course_id'
+
+    def get_queryset(self):
+        return Course.objects.filter(id=self.kwargs.get('course_id'))
+
+    def get_context_data(self, **kwargs):
+        context = super(CourseDetailView, self).get_context_data(**kwargs)
+        context['lessons'] = Lesson.objects.filter(id=self.kwargs.get('course_id'))
+        return context
 
 
 def enroll(request, course_id):
