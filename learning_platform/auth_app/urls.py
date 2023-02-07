3951cb8bb4222274d3d72bbde0a6484d0b40b143
OutlyingWest <alexeybuv7@amail.com>
2023-02-07 15:16:20 +0300

    change password feature added

--- learning_platform/auth_app/urls.py
+++ learning_platform/auth_app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.views import (LoginView, LogoutView,
                                        PasswordResetView, PasswordResetDoneView,
-                                       PasswordResetConfirmView, PasswordResetCompleteView)
+                                       PasswordResetConfirmView, PasswordResetCompleteView,
+                                       PasswordChangeView, PasswordChangeDoneView)
 from django.urls import path
 from . import views
 
@@ -8,11 +9,13 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('change-password/', views.change_password, name='change_password'),
+    # Controllers for change password
+    path('password_change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
     # Controllers for reset password
     path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
     path('password-reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uuid64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
