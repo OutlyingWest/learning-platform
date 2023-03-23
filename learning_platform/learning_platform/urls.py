6ea458dda1c765796978ccd5203a248f0c6882b0
OutlyingWest <alexeybuv7@amail.com>
2023-03-23 11:30:44 +0300

    debugtoolbar url deleted

--- learning_platform/learning_platform/urls.py
+++ learning_platform/learning_platform/urls.py
@@ -27,8 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auth_app.urls')),
     path('courses/', include('learning.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
-
 ]
 
 if settings.DEBUG:
