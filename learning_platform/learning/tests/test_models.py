7f2d90ae91e5979574fd47be6bfe18362b86e968
OutlyingWest <alexeybuv7@amail.com>
2023-04-02 10:29:15 +0300

    learning tests done at all

--- learning_platform/learning/tests/test_models.py
+++ learning_platform/learning/tests/test_models.py
@@ -10,6 +10,7 @@ class LearningModelsTestCase(TestCase):
         course = Course.objects.get(title='Go')
         self.assertEqual(str(course), f'{course.title}: Старт {course.start_date}')
 
+    @tag('lesson_to_str')
     def test_lesson_to_str(self):
         lesson = Lesson.objects.get(name='1й Go')
-        self.assertEqual(str(lesson), f'{lesson.course.title}: Урок {lesson.name}')
+        self.assertEqual(str(lesson), f'{lesson.course.title}: Старт {lesson.course.start_date}: Урок {lesson.name}')
